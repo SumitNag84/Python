f23d0c5c48b954ec51c26d4bd09375751d0e6dbe
SumitNag84 <sumit.nagrale74@gmail.com>
2025-04-01 21:37:13 +0530

    Add files via upload

---
+++ Assignment1Task1.py
@@ -0,0 +1,21 @@
+#Task 1: Perform Basic Mathematical Operations
+# Basic Mathematical operations
+
+a=input('enter first number: ')
+b=input('enter second number: ')
+a=float(a)
+b=float(b)
+Sum=(a+b)
+Sum=int(Sum)
+Sub=(a-b)
+Sub=int(Sub)
+Mult=(a*b)
+Mult=int(Mult)
+Div=(a/b)
+Div=float(Div)
+
+print('Addition of 2 numbers',Sum)
+print('Subtraction of 2 numbers',Sub)
+print('Multiplication of 2 numbers',Mult)
+print('Division of 2 numbers',Div)
+
---
+++ Assignment1Task2.py
@@ -0,0 +1,8 @@
+#Task 2: Create a Personalized Greeting
+# Basic Mathematical operations
+
+a=input('enter your first name: ')
+b=input('enter your last name: ')
+
+concate=a+" " +b
+print('Hello '+ concate + '! Welcome to the Python Program.')
