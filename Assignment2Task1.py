b416234a4ac7083c9bc9805d11fba07df22914c6
SumitNag84 <sumit.nagrale74@gmail.com>
2025-04-23 15:18:53 +0530

    Assignment 2 task 1 and task 2 uploaded

---
+++ Assignment2Task1.py
@@ -0,0 +1,8 @@
+#Ass 2 Task 2:
+
+a=input('Enter a number: ')
+b = int(a)
+if b%2 == 0:
+    print(a + ' is even number')
+else:
+    print(a + ' is odd number')
---
+++ Assignment2Task2.py
@@ -0,0 +1,9 @@
+#Ass 2 Task 2:
+
+sum = 0
+
+for i in range(1,51):
+    sum =sum+i
+    if i==50:
+        print("Sum of integer from  1 to 50 is: " + str(sum))
+        break
